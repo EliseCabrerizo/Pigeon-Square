f90556f49b2a01b8037b277aa2408ef79c7d7729
Sandrine <sandrine.yven@gmail.com>
2018-10-19 13:52:12 -0400

    fenetre et background

--- src/Pigeon.java
+++ src/Pigeon.java
@@ -1,4 +1,29 @@
 
 public class Pigeon {
 
+	private int posX;
+	private int posY;
+	private int vitesse;
+	
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	public int getVitesse() {
+		return vitesse;
+	}
+	public void setVitesse(int vitesse) {
+		this.vitesse = vitesse;
+	}
+	
+	
+	
 }
